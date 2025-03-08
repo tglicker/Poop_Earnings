0ef1fe2788350663665a4f91c4363ff14423add4
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-07 19:28:03 -0600

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent"; // Ensure this is correctly imported
+import PoopTracker from "./components/PoopTracker";
 
 function App() {
   return (
     <div>
       <Header />
-      <h2>Welcome to the Poop Earnings Tracker!</h2>
-      <MainContent />  {/* Make sure this is included */}
+      <PoopTracker />  {/* Make sure this is included */}
       <Footer />
     </div>
   );
