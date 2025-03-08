f95ab3b06415e432ee64023d57bbeefed07808df
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-07 18:08:33 -0600

    Create Footer.js

---
+++ FrontEnd/src/components/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Poop Earnings Tracker</p>
+        </footer>
+    );
+}
+
+export default Footer;
