75b570ef9f43613c2b6026ebd92f8eb255333ba7
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-07 19:22:39 -0600

    Create MainContent.js

---
+++ FrontEnd/src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main>
+      <h2>Welcome to the Poop Earnings Tracker!</h2>
+      <p>Start tracking your bathroom breaks and see how much money you're making while on the clock!</p>
+    </main>
+  );
+}
+
+export default MainContent;
