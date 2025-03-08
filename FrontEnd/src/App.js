422c78d988feccea2c2894e1c420460efaea5443
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-07 18:09:13 -0600

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,2 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <h2>Welcome to the Poop Earnings Tracker!</h2>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
 cd FrontEnd/src
 ls
