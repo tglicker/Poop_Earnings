b783b7b42568afac373031ce75bdb6e83b901c49
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-07 19:20:28 -0600

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MainContent from "./components/MainContent"; // Ensure this is correctly imported
 
 function App() {
-    return (
-        <div className="container">
-            <Header />
-            <h2>Welcome to the Poop Earnings Tracker!</h2>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to the Poop Earnings Tracker!</h2>
+      <MainContent />  {/* Make sure this is included */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
