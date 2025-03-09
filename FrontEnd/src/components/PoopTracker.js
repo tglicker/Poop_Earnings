72ab7474c2f39f6d1657b938a7ace3becfa4725a
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-09 09:51:16 -0500

    Update PoopTracker.js

--- FrontEnd/src/components/PoopTracker.js
+++ FrontEnd/src/components/PoopTracker.js
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
 import SalaryInput from "./SalaryInput";
-import PoopLog from "./PoopLog";
-import EarningsDisplay from "./EarningsDisplay";
 import PoopTimer from "./PoopTimer";
-
+import EarningsDisplay from "./EarningsDisplay";
+import PoopLog from "./PoopLog";
 
 const PoopTracker = () => {
   const [salary, setSalary] = useState(0);
-  const [totalEarnings, setTotalEarnings] = useState(0);
+  const [totalPoopTime, setTotalPoopTime] = useState(0); // Total minutes
+  const [poopLog, setPoopLog] = useState([]); // Stores past poops
+
+  const handleSalaryChange = (newSalary) => {
+    setSalary(newSalary);
+  };
 
   const logPoopSession = (minutes) => {
-    if (salary > 0) {
-      const earningsPerMinute = salary / (40 * 60); // Assuming 40-hour work week
-      setTotalEarnings(totalEarnings + earningsPerMinute * minutes);
-    }
+    const earnings = (salary / 2080) * minutes; // Calculate based on yearly salary & work hours
+    setTotalPoopTime((prevTime) => prevTime + minutes);
+    
+    setPoopLog((prevLog) => [
+      ...prevLog,
+      { date: new Date().toLocaleString(), minutes, earnings },
+    ]);
   };
 
   return (
     <div>
       <h1>Poop Earnings Tracker</h1>
-      <SalaryInput salary={salary} setSalary={setSalary} />
-      <PoopLog logPoopSession={logPoopSession} />
-      <EarningsDisplay totalEarnings={totalEarnings} />
+      <SalaryInput onSalaryChange={handleSalaryChange} />
+      <PoopTimer salary={salary} logPoopSession={logPoopSession} />
+      <EarningsDisplay salary={salary} totalPoopTime={totalPoopTime} />
+      <PoopLog log={poopLog} />
     </div>
   );
 };
