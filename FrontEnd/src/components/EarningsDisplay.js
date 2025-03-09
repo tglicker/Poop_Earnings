41248dc02e56eb80cb212db287864158729a2ce9
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-09 11:42:53 -0500

    Update EarningsDisplay.js

--- FrontEnd/src/components/EarningsDisplay.js
+++ FrontEnd/src/components/EarningsDisplay.js
@@ -3,11 +3,8 @@ import React from 'react';
 const EarningsDisplay = ({ totalEarnings, currentSessionEarnings }) => {
   return (
     <div className="earnings-display">
-      <h2>Current Session Earnings</h2>
-      <p>${currentSessionEarnings.toFixed(2)}</p>
-
-      <h2>Total Earned While Pooping</h2>
-      <p>${totalEarnings.toFixed(2)}</p>
+      <h2>Current Session Earnings: ${currentSessionEarnings.toFixed(2)}</h2>
+      <h2>Total Earned While Pooping: ${totalEarnings.toFixed(2)}</h2>
     </div>
   );
 };
