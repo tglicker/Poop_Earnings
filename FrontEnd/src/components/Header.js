620eebd71904817fb6cd8e99f54d70c3e0698da4
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-09 10:46:28 -0500

    Update Header.js

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <h1>Poop Earnings Tracker</h1>
+            //<h1>Poop Earnings Tracker</h1>
         </header>
     );
 }
