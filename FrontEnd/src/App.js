aa35bb1c4eba1db4f9a05e9ad009a3c8a95136c8
tglicker <31174980+tglicker@users.noreply.github.com>
2025-03-09 20:31:58 -0500

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import PoopTracker from "./components/PoopTracker";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="app-container">
       <PoopTracker />
-      <Footer />
     </div>
   );
 }
